9ea5aa4a0da581b4ce3bfed48903e39e696d559d
Satmeet Singh <satmeetarora@gmail.com>
2025-02-26 15:40:43 +0530

    feat:Created Contact Conponent

---
+++ src/Utils/GoogleMap/GoogleMap.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GoogleMap: React.FC = () => {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d214.35355422392058!2d76.749571087389!3d30.72804541088627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fedefeca05da7%3A0x488ef40b89968276!2sApurva%20Arts!5e0!3m2!1sen!2sin!4v1740552278504!5m2!1sen!2sin"
+      width="100%"
+      height="300"
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    />
+  );
+};
+
+export default GoogleMap;
