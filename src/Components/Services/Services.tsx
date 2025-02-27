70c4b949f06bed873fb81d9eef30e7a26724e1ff
Satmeet Singh <satmeetarora@gmail.com>
2025-02-27 18:29:41 +0530

    feat : Added hover to card and outline none to input

--- src/Components/Services/Services.tsx
+++ src/Components/Services/Services.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import Card from "./Card/Card";
-import styles from "./services.module.css";
-import servicesData from "../../assets/services.json";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { useEffect, useState } from 'react';
+import Card from './Card/Card';
+import styles from './services.module.css';
+import servicesData from '../../assets/services.json';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 export interface Service {
   id: number;
   name: string;
@@ -43,17 +43,17 @@ export default function Services() {
           <span className={styles.icon}>
             <ArrowForwardIcon
               sx={{
-                fontSize: "30px",
+                fontSize: '30px',
               }}
             />
           </span>
           <span className={styles.btntext} onClick={toggleServices}>
-            {showAll ? "Show Less" : "Show More"}
+            {showAll ? 'Show Less' : 'Show More'}
           </span>
         </button>
         <div className={styles.cardbox}>
           {visibleServices.map((service: Service) => (
-            <div key={service.id}>
+            <div key={service.id} className={styles.cards}>
               <Card service={service} />
             </div>
           ))}
