674571eca2721a1d73bac96e7b49a727bb1e8eca
NishantK2001 <nishant.k28101@gmail.com>
2025-03-03 18:06:48 +0530

    feat: Added images for business cards and fix navbar

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,25 +1,29 @@
-import Contact from '../../Components/Contact/Contact';
-import Hero from '../../Components/Hero/Hero';
-import Services from '../../Components/Services/Services';
-import { Element } from 'react-scroll';
-import AboutUsTwo from '../../Components/AboutUsTwo/AboutUsTwo';
-import styles from './homepage.module.css';
+import Contact from "../../Components/Contact/Contact";
+import Hero from "../../Components/Hero/Hero";
+import Services from "../../Components/Services/Services";
+import { Element } from "react-scroll";
+import AboutUsTwo from "../../Components/AboutUsTwo/AboutUsTwo";
+import styles from "./homepage.module.css";
+import Navbar from "../../Components/Navbar/Navbar";
 
 export default function HomePage() {
   return (
-    <div className={styles.home}>
-      <Element name="home">
-        <Hero />
-      </Element>
-      <Element name="services">
-        <Services />
-      </Element>
-      <Element name="about">
-        <AboutUsTwo />
-      </Element>
-      <Element name="contact">
-        <Contact />
-      </Element>
-    </div>
+    <>
+      <Navbar />
+      <div className={styles.home}>
+        <Element name="home">
+          <Hero />
+        </Element>
+        <Element name="services">
+          <Services />
+        </Element>
+        <Element name="about">
+          <AboutUsTwo />
+        </Element>
+        <Element name="contact">
+          <Contact />
+        </Element>
+      </div>
+    </>
   );
 }
